6b13fdf02b8ab3e92c22fadbf549860629690bc6
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 22:17:53 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     if song.artist
       link_to song.artist.name, artist_path(song.artist)
     else
-      link_to song.title, edit_song_path(song)
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
