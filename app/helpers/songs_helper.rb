98c4121540da42860ceb86ebb9259b33629e51dd
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 22:06:06 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
   def display_artist(song)
     if song.artist
-      link to artists show page
+      link_to song.artist, :controller => artist_controller, :action => show
     else
       link to the song's edit page
     end
