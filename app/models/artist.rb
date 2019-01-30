ad34207c8ae5911960a79720948483a34716c073
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 21:50:27 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,8 +2,10 @@ class Artist < ActiveRecord::Base
   has_many :songs
 
   def artist_name
+    self.name
   end
 
-  def artist_name=
+  def artist_name=(value)
+    self.artist_name = value
   end
 end
