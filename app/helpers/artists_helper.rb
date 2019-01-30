4e24cb4dda523d3f74d0088cd9721753a1c78e4c
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 22:07:39 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist(song)
+    if song.artist
+      link_to song.artist, :controller => artist_controller, :action => show
+    else
+      link_to song, :controller => song_controller, :action => show
+    end
+  end
 end
