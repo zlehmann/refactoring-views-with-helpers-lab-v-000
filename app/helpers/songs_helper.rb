00fe61ef36ef13075ecaa102ed51dcb8f65f3d77
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 22:01:43 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,9 @@
 module SongsHelper
-  def display_artist(value)
-
+  def display_artist(song)
+    if song.artist
+      link to artists show page
+    else
+      link to the song's edit page
+    end
   end
 end
