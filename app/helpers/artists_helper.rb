6f085ca363266f2241652bc3d30eda431e96c222
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 22:16:31 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,9 @@
 module ArtistsHelper
   def display_artist(song)
     if song.artist
-      link_to song.artist
+      link_to song.artist.name, artist_path(song.artist)
     else
-      link_to song, :action => "show"
+      link_to song.name, song_path(song)
     end
   end
 end
